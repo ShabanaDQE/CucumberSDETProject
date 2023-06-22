1cc886ce595aed9ccc771038eaa52fa12d7795b1
ShabanaDQE <shabanaqe1989@gmail.com>
2023-06-22 21:22:13 +0200

    BDDFramework Scripts

---
+++ CucumberSDETProject-master/test-output/report.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:Features/Login1.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Successful Login with Valid Credentials",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@sanity"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User Launch Chrome browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Stepdef.user_Launch_Chrome_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User opens URL \"http://admin-demo.nopcommerce.com/login\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Stepdef.user_opens_URL(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User enters Email as \"admin@yourstore.com\" and Password as \"admin\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Stepdef.user_enters_Email_as_and_Password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Login",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Stepdef.click_on_Login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Page Title should be \"Dashboard / nopCommerce administration\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdef.page_Title_should_be(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User click on Log out link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Stepdef.user_click_on_Log_out_link()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Page Title should be \"Your store. Login\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Stepdef.page_Title_should_be(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "close browser",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Stepdef.close_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
